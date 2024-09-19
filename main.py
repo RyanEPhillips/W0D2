3c7b59ef2faefeef1b1a92f3c23659a18414f8b9
Ryan <85RyanP@gmail.com>
2024-09-19 15:00:01 -0400

    Add greet_person function to main.py, Modified test_main.py to test new function

--- main.py
+++ main.py
@@ -2,5 +2,10 @@
 def hello_world():
     return "Hello from Ryan Phillips"
 
+def greet_person(name):
+    return f"Hello, {name}!"
+
 if __name__ == "__main__":
+    name = input("What is your name? ")
     print(hello_world())
+    print(greet_person(name))
--- test_main.py
+++ test_main.py
@@ -1,10 +1,15 @@
 # test_main.py
 import unittest
-from main import hello_world
+from main import hello_world, greet_person
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello from Ryan Phillips")
+
+class TestGreetPerson(unittest.TestCase):
+    def test_greet_person(self):
+        name=''
+        self.assertEqual(greet_person(name), f"Hello, {name}!")
 
 if __name__ == '__main__':
     unittest.main()
